f81e7426d759e6978a8b24a9a6171104e1b2b38f
Paulo Aguiar Junior <juuninho@gmail.com>
2021-07-26 11:16:41 -0300

    SellerService and basic findAll

---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> findAll()
+        {
+            //sincrona por enquanto
+            return _context.Seller.ToList();
+        }
+
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc
 {
@@ -41,6 +42,7 @@ namespace SalesWebMvc
 
             //registra o serviço
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
 
 
         }
